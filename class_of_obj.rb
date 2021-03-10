7f96c7e5197fe6ebd852b5d192f7760cbfbac205
EC2 Default User <ec2-user@ip-172-31-48-52.ec2.internal>
2021-03-10 13:07:31 +0000

    class,method復習

---
+++ class_of_obj.rb
@@ -0,0 +1,5 @@
+puts 1.class
+puts 1.08.class
+puts "a".class
+puts [1,2].class
+puts ({a:1}.class)
---
+++ new_method.rb
@@ -0,0 +1,16 @@
+def calc_triangle_area (base, height)
+  base * height / 2
+end
+
+puts calc_triangle_area(10, 5)
+
+def calc_triangle_area(base, height)
+  base * height / 2
+end
+
+mybase = 12
+myheight = 3
+
+puts calc_triangle_area(mybase, myheight)
+
+# メソッドは定義してから呼び出す。
